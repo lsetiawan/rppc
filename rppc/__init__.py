5ae8312bba068c84aac31bbbbb38c20990f376fd
Landung Setiawan <landungs@uw.edu>
2018-07-19 11:39:48 -0700

    "Initialize package repository"

---
+++ rppc/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'Landung Setiawan'
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ tests/test_example.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def test_hello_world():
+    text = 'Hello World'
+    assert text == 'Hello World'
